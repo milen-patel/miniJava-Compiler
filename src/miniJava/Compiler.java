5c6dc80d5361e9b5d86d88b9f373c08119e2cc20
Milen <milen@live.unc.edu>
2022-04-24 17:05:24 -0400

    Has code to run pa1-pa3 tests

--- src/miniJava/Compiler.java
+++ src/miniJava/Compiler.java
@@ -7,12 +7,110 @@ import java.io.PrintStream;
 import mJAM.Disassembler;
 import mJAM.Interpreter;
 import mJAM.ObjectFile;
+import miniJava.AbstractSyntaxTrees.ASTDisplay;
 import miniJava.AbstractSyntaxTrees.Package;
 import miniJava.SyntacticAnalyzer.InputReader;
 import miniJava.SyntacticAnalyzer.Scanner;
 import miniJava.SyntacticAnalyzer.Parser;
-
+import java.io.*;
 public class Compiler {
+	
+	public static void runPA1Tests() {
+		String path = "/Users/milenpatel/Downloads/pa1_tests";
+		File folder = new File(path);
+		PrintStream defaultPrintStream = System.out;
+		File[] listOfFiles = folder.listFiles();
+		// Iterate Over Each File
+				for (int i = 0; i < listOfFiles.length; i++) {
+				  if (listOfFiles[i].isFile() && listOfFiles[i].getName().endsWith(".java") && listOfFiles[i].getName().startsWith("pass")) {
+					  
+					InputReader reader = new InputReader(listOfFiles[i].getAbsoluteFile().toString());
+					Scanner scnr = new Scanner(reader);
+					Parser p = new Parser(scnr);
+					try {
+						Package tree = p.parseProgram();			
+						
+						// Once we finish AST Display, change the output stream back
+						System.setOut(defaultPrintStream);
+
+						// Take the diff of the file with the expected output
+						System.out.println("Passed As Expected!");
+					} catch (Exception e)  {
+						System.out.println("FAIL on " + listOfFiles[i].getAbsolutePath());
+					}
+				  } else if (listOfFiles[i].getName().endsWith(".java") && listOfFiles[i].getName().startsWith("fail")){
+					  // Fails
+					  try {
+						  InputReader reader = new InputReader(listOfFiles[i].getAbsoluteFile().toString());
+						    System.setOut(new PrintStream(new OutputStream() { public void write(int b) {} }));
+						  	Scanner scnr = new Scanner(reader);
+							Parser p = new Parser(scnr);
+							Package tree = p.parseProgram();
+							
+							System.setOut(defaultPrintStream);
+						  System.out.println("Should have failed but didn't " + listOfFiles[i].getAbsoluteFile());
+					  } catch (Exception e) {
+						  System.setOut(defaultPrintStream);
+						  System.out.println("Failed as expected!");
+					  }
+				  }
+				}		
+	}
+	
+	public static void runPA2Tests() {
+		File folder = new File("/Users/milenpatel/Downloads/pa2_tests");
+		PrintStream defaultPrintStream = System.out;
+		File[] listOfFiles = folder.listFiles();
+		
+		// Iterate Over Each File
+		for (int i = 0; i < listOfFiles.length; i++) {
+		  if (listOfFiles[i].isFile() && listOfFiles[i].getName().endsWith(".java") && listOfFiles[i].getName().startsWith("pass")) {
+			// Open a dummy file to write the AST Into
+			try {
+				System.setOut(new PrintStream(new BufferedOutputStream(new FileOutputStream("/Users/milenpatel/Downloads/pa2_tests/text.txt")), true));
+			} catch (FileNotFoundException e1) { e1.printStackTrace();}
+			
+			// Create the AST and write it to teh file
+			System.out.println("======= AST Display =========================");
+			InputReader reader = new InputReader(listOfFiles[i].getAbsoluteFile().toString());
+			Scanner scnr = new Scanner(reader);
+			Parser p = new Parser(scnr);
+			try {
+				Package tree = p.parseProgram();			
+				
+				//System.setOut(defaultPrintStream);
+				//System.out.println(listOfFiles[i].getPath().substring(38));
+				//(new miniJava.ContextualAnalyzer.Identification()).visitPackage(tree, null);
+				//(new miniJava.ContextualAnalyzer.TypeChecker()).visitPackage(tree, null);
+				(new ASTDisplay()).visitPackage(tree, "");
+				System.out.println("=============================================");
+				
+				// Once we finish AST Display, change the output stream back
+				System.setOut(defaultPrintStream);
+
+				// Take the diff of the file with the expected output
+				Process pro = Runtime.getRuntime().exec(new String[]{"zsh","-c","diff " + listOfFiles[i].getAbsoluteFile().toString() + ".out /Users/milenpatel/Downloads/pa2_tests/text.txt"});
+				if (pro.waitFor() == 1) {
+					System.out.println("Different AST than what was expected for " + listOfFiles[i].getAbsolutePath().toString());
+				} else {  }
+			} catch (Exception e)  {
+				System.out.println("FAIL");
+			}
+		  } else if (listOfFiles[i].getName().endsWith(".java") && listOfFiles[i].getName().startsWith("fail")){
+			  // Fails
+			  try {
+				  InputReader reader = new InputReader(listOfFiles[i].getAbsoluteFile().toString());
+				  System.setOut(new PrintStream(new OutputStream() { public void write(int b) {} }));
+				  	Scanner scnr = new Scanner(reader);
+					Parser p = new Parser(scnr);
+					Package tree = p.parseProgram();
+					System.setOut(defaultPrintStream);
+				  System.out.println("Should have failed but didn't " + listOfFiles[i].getAbsoluteFile());
+			  } catch (Exception e) {
+				  System.setOut(defaultPrintStream);
+			  }
+		  }}
+		}
 	
 	static void runPA3Tests() {
 		String path = "/Users/milenpatel/Downloads/pa3_tests";
@@ -60,26 +158,10 @@ public class Compiler {
 	}
 	
 	public static void main(String[] args) {
-		args[0] = "/Users/milenpatel/git/miniJava-Compiler/test/Code Generation/BlockStatements.java";
-		//args[0] = "/Users/milenpatel/git/miniJava-Compiler/test/Code Generation/CallStatementStackUsage.java";
-		//args[0] = "/Users/milenpatel/git/miniJava-Compiler/test/Code Generation/Arrays.java";
-		//args[0] = "/Users/milenpatel/git/miniJava-Compiler/test/Code Generation/References.java";
-		//args[0] = "/Users/milenpatel/git/miniJava-Compiler/test/Code Generation/NameCovering.java";
-		//args[0] = "/Users/milenpatel/git/miniJava-Compiler/test/Code Generation/ReturningThis.java";
-		//args[0] = "/Users/milenpatel/git/miniJava-Compiler/test/Code Generation/BinaryTree.java";
-		//args[0] = "/Users/milenpatel/git/miniJava-Compiler/test/Code Generation/Quicksort.java";
-		//args[0] = "/Users/milenpatel/git/miniJava-Compiler/test/Code Generation/StaticCall.java";
-		//args[0] = "/Users/milenpatel/git/miniJava-Compiler/test/Code Generation/Recursion.java";
-		//args[0] = "/Users/milenpatel/git/miniJava-Compiler/test/Code Generation/AssignStatements.java";
-		//args[0] = "/Users/milenpatel/git/miniJava-Compiler/test/Code Generation/GeneralChecks.java";
-		//args[0] = "/Users/milenpatel/git/miniJava-Compiler/test/Code Generation/ArrayConcatenation.java";
-		//args[0] = "/Users/milenpatel/git/miniJava-Compiler/test/Code Generation/MultipleClasses.java";
-		//args[0] = "/Users/milenpatel/git/miniJava-Compiler/test/Code Generation/GenericPass2.java";
-		//args[0] = "/Users/milenpatel/git/miniJava-Compiler/test/Code Generation/GenericPass3.java";
-		//args[0] = "/Users/milenpatel/git/miniJava-Compiler/test/Code Generation/GenericPass4.java";
-
+		runPA1Tests();
+		//runPA2Tests();
 		//runPA3Tests();
-		//System.exit(3);
+		System.exit(3);
 		
 		
 		if (args == null || args.length != 1) {
